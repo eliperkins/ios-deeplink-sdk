1a221c8f4c71c9a8f3de6c19b69b0aa4d640b438
Eli Perkins <eli.perkins@venmo.com>
2015-02-26 17:35:06 -0500

    Create a framework target and move files to it

--- DeepLinkSDK/DeepLinkSDK.h
+++ DeepLinkSDK/DeepLinkSDK.h
@@ -1,6 +1,17 @@
-#import "DPLTargetViewControllerProtocol.h"
-#import "DPLDeepLinkRouter.h"
-#import "DPLRouteHandler.h"
-#import "DPLDeepLink.h"
-#import "DPLMutableDeepLink.h"
-#import "DPLErrors.h"
+#import <Foundation/Foundation.h>
+
+//! Project version number for DeepLinkSDK.
+FOUNDATION_EXPORT double DeepLinkSDKVersionNumber;
+
+//! Project version string for DeepLinkSDK.
+FOUNDATION_EXPORT const unsigned char DeepLinkSDKVersionString[];
+
+#import <DeepLinkSDK/DPLTargetViewControllerProtocol.h>
+#import <DeepLinkSDK/DPLDeepLinkRouter.h>
+#import <DeepLinkSDK/DPLRouteHandler.h>
+#import <DeepLinkSDK/DPLRouteMatcher.h>
+#import <DeepLinkSDK/DPLDeepLink.h>
+#import <DeepLinkSDK/DPLMutableDeepLink.h>
+#import <DeepLinkSDK/DPLErrors.h>
+
+#import <DeepLinkSDK/AppLinks.h>
